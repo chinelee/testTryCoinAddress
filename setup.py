33f8fc2ceb88c1e830c4a2d0967ed01d5d7c5cb9
chinelee <lclunyu100@Gmail.com>
2017-07-27 15:46:01 +0800

    chine

---
+++ setup.py
@@ -0,0 +1,15 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='btcaddr',
+    version='1.0',
+    description="Generate trycoin addresses.",
+    author="chinelee",
+    author_email='lclunyu100@gmail.com',
+    url='https://github.com/chinelee/testTryCoinAddress',
+    scripts=['testtcoaddr'],
+    license='LICENSE',
+)
