4172be6816d1ac6fb2446cb7825d1e76dd7354c3
ChineLee <lclunyu100@gmail.com>
2017-07-27 18:02:32 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name='testtcoaddr',
+    name='tcoaddress',
     version='1.0',
     description="Generate trycoin addresses.",
     author="chinelee",
     author_email='lclunyu100@gmail.com',
     url='https://github.com/chinelee/testTryCoinAddress',
-    scripts=['testtcoaddr'],
+    scripts=['tcoaddress'],
     license='LICENSE',
 )
